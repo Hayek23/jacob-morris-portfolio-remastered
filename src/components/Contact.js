4b63aa6eca25c3049d7e3ef66cd99a05b286b613
jwm <jwm3926@gmail.com>
2023-01-30 17:45:46 -0500

    finished contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,36 @@
+const styles ={
+    textarea: {
+        width: '23rem',
+        height: '10rem',
+        margin: '.5rem',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'block',
+    },
+    topBoxes: {
+        margin: '1rem',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    h1: {
+        textAlign: 'center'
+    }
+}
+
 export default function Contact(){
     return(
         <div>
-            <h1>Contact Me</h1>
+            <h1 style={styles.h1}>Contact Me</h1>
             <form>
-                <input placeholder = 'Please enter your name!'/>
-                <input placeholder = 'Please enter your email here!'/>
-                <textarea placeholder = 'Please enter your message here!'/>
-                <button>Submit!</button>
+                <input  style={styles.topBoxes} placeholder = 'Please enter your name!'/>
+                <input  style={styles.topBoxes} placeholder = 'Please enter your email here!'/>
+            </form>
+            <form>
+            <textarea style={styles.textarea} placeholder = 'Please enter your message here!'/>
+            </form>
+            <form>
+            <button>Submit!</button>
             </form>
         </div>
     )
