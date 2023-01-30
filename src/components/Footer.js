c7e5038997f9a941a0359bb0e1d084763a47b9be
jwm <jwm3926@gmail.com>
2023-01-29 19:22:07 -0500

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import linkedinLogo from '../assets/linkedinLogo.png'
+import githubLogo from '../assets/githubLogo.png'
+
 const styles={
     footer: {
         position: 'absolute',
@@ -5,12 +8,24 @@ const styles={
         width: '100%',
         height: '2.5rem',
         background: 'grey'
+    },
+    img: {
+        height: '35px',
+        width: '35px',
+        padding: '.25rem'
     }
 }
 
 export default function Footer(){
     return(
-        <footer style={styles.footer}>footer</footer>
+        <footer style={styles.footer}>
+            <a href= 'https://github.com/Hayek23'>
+                <img style= {styles.img} src= {githubLogo}/>
+            </a>
+            <a href= 'https://www.linkedin.com/in/jacob-morris-ba2086194/'>
+                <img style= {styles.img} src= {linkedinLogo}/>
+            </a>
+        </footer>
     )
 }
 
