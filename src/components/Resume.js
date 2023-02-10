8fc3e7617886163e3593af4fd5396868ba574e40
jwm <jwm3926@gmail.com>
2023-02-10 09:16:07 -0500

    finished resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,31 @@
+const styles = {
+    h1: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    btn: {
+        display: 'flex',
+        margin: 'auto'
+    }
+}
+
 export default function Resume() {
+    const onButtonClick = () => {
+        fetch('Jacob Morris Resume.pdf').then(res => {
+            res.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Jacob Morris Resume.pdf';
+                alink.click();
+            })
+        })
+    }
     return(
-        <div> Resume </div>
+        <div>
+            <h1 style={styles.h1}> Resume </h1>
+            <button style = {styles.btn} onClick={onButtonClick}>Download My Resume</button>
+        </div>
     )
 }
